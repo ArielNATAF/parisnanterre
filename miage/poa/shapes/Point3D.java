ebac59107132a9c81b0ffabdef7bd14a6f57ca39
Ariel NATAF <nataf.ariel@gmail.com>
2017-09-12 20:24:38 +0200

    Inquisitorial

--- miage/poa/shapes/Point3D.java
+++ miage/poa/shapes/Point3D.java
@@ -1,7 +1,12 @@
 package fr.parisnanterre.miage.poa.shapes;
 
 /**
- * Created by arielnataf on 11/09/2017.
+ * Created by arielnataf on 11/09/2017.											 ============/    \============
+ * This code is protected by the Light of the God-Emperor of Mankind.			-\  -----  | _    _ |  -----  /-
+ * Even in his almost-Life, sitting on the Golden Throne, His mind must			  -\  ---  | \)  (/ |  ---  /-
+ remain vigilant at all times throughout the entire Imperium 					    -   --  \  ,,  / --   -
+ to safeguard the human race and to offer His protection to the faithful.  			  ------- |||| -------
+ * Therefor, I, arielnataf, sing His sacred Psalms. Heretic beware His wrath.			      ||||
  */
 public class Point3D extends Point2D {
     public double z;
